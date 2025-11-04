87ddd579b6489824fe81627dba1526e38ff166a1
filipedv <filiped@uia.no>
2025-11-04 12:09:11 +0100

    Endret slik at port som åpnes er en https 8443 port. Det funker ikke å åpne den via docker så man må skrive: https://localhost:8443 i nettleseren

--- OBLIG1/OBLIG1-Prosjekt/Program.cs
+++ OBLIG1/OBLIG1-Prosjekt/Program.cs
@@ -33,8 +33,9 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
-    app.UseHttpsRedirection();
 }
+
+app.UseHttpsRedirection();
 
 app.UseRouting();
 
