4b84b61e156337ea961220ab963dc1f41204d08c
idaelisej <ijamissen@yahoo.com>
2025-11-06 09:34:23 +0100

    Laget registerfører side

---
+++ OBLIG1/OBLIG1-Prosjekt/Controllers/RegisterforerController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OBLIG1.Controllers
+{
+    public class RegisterforerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+    
