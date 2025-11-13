d6e2a66371cca06580c9ec33472e6ab60f8a9128
filipedv <filiped@uia.no>
2025-11-13 13:02:55 +0100

    Prøvd å legge til nye migrasjoner

---
+++ OBLIG1/OBLIG1-Prosjekt/Migrations/20251108161942_AddObstacleType.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OBLIG1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddObstacleType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Height",
+                table: "Obstacles",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Obstacles",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Obstacles");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Height",
+                table: "Obstacles",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+        }
+    }
+}
