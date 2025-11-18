47fae22a8ca7a38f606ceae01d364dd2a09e1dc4
siljesolberg <siljeso@uia.no>
2025-11-18 12:23:52 +0100

    ObstacleNameTest

---
+++ OBLIG1/OBLIG1-Prosjekt/OBLIG1.Tests/DefaultObstacleNameTest.cs
@@ -0,0 +1,14 @@
+﻿using OBLIG1.Models;
+using Xunit;
+
+namespace OBLIG1.Tests;
+
+public class DefaultObstacleNameTest
+{
+    [Fact]
+    public void Obstacle_DefaultName_WhenNameMissing()
+    {
+        var o = new Obstacle { Name = "" };
+        Assert.True(string.IsNullOrWhiteSpace(o.Name) || o.Name == "");
+    }
+}
