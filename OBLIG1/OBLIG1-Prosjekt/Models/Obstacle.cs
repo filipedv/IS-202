49ace6a75bb372f5dd9d16203106c62d6573b533
OyvindBirkeland <oyvind.birkeland2@hotmail.com>
2025-11-21 13:51:10 +0100

    Dokumenstasjon til models
    Skrevet kommentarer

--- OBLIG1/OBLIG1-Prosjekt/Models/Obstacle.cs
+++ OBLIG1/OBLIG1-Prosjekt/Models/Obstacle.cs
@@ -5,19 +5,24 @@ namespace OBLIG1.Models
 {
     public class Obstacle
     {
+        //primærnøkkel for databasen
         public int Id { get; set; }
-
+        //Navn på hinderet, maks lengde 100 tegn
         [Required, StringLength(100)]
         public string Name { get; set; } = "";
-
+        
+        //høyde på hinderet, kan være fra 0 til 200
         [Range(0, 200)]
         public double? Height { get; set; }
-
+        
+        //Beskrivelse av hinderet, kan være opptil 1000 tegn
         [StringLength(1000)]
         public string? Description { get; set; }
-
+        
+        //Angir om hinderet er i kladde modus
         public bool IsDraft { get; set; }
 
+        //ID til brukeren som opprettet hinderet
         [StringLength(256)]
         public string? CreatedByUserId { get; set; }
 
@@ -25,13 +30,18 @@ namespace OBLIG1.Models
         [ForeignKey(nameof(CreatedByUserId))]
         public ApplicationUser? CreatedByUser { get; set; }
 
+        //Status til hinderet. Kan være Godkjent, Avvist eller pending
         public ObstacleStatus Status { get; set; }
 
+        //Lagrer geometri-data i GeoJson-format
+        //Longtext sikrer plass til store mengder tekst
         [Column(TypeName = "longtext")]
         public string? GeometryGeoJson { get; set; }
 
+        //Tidspunk for registrering
         public DateTime RegisteredAt { get; set; }
         
+        //hindertype
         public string? Type { get; set; }
     }
 }
--- OBLIG1/OBLIG1-Prosjekt/Models/ObstacleData.cs
+++ OBLIG1/OBLIG1-Prosjekt/Models/ObstacleData.cs
@@ -3,16 +3,20 @@ using System.ComponentModel.DataAnnotations;
 namespace OBLIG1.Models;
 public class ObstacleData
 {
+    //navn på hinderet, max 100 tegn
     [MaxLength(100)]
     public string ObstacleName { get; set; }
 
+    //Påkrevd felt. Må være mellom 0-200
     [Required(ErrorMessage = "Field is required")]
     [Range(0, 200)]
     public double ObstacleHeight { get; set; }
 
+    //beskrivelse av hinderet. Max 1000 tegn
     [MaxLength(1000)]
     public string ObstacleDescription { get; set; }
 
+    //Angir om det er kladd eller ikke
     public bool IsDraft { get; set; }
 
     
